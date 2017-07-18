d540df49a7c4c41868a86953a91cd4aff949063d
Ewan Breakey <ebreakey@gmail.com>
2017-07-18 20:59:24 +1000

    Update itheft

--- r/comp_sac_itheft/second.js
+++ r/comp_sac_itheft/second.js
@@ -1,5 +1,13 @@
 /* global $ */
 $(() => {
+  window.goBack = () => {
+    $('*').animate({
+      opacity: 0
+    }, 400, () => {
+      window.location = 'index.html'
+    })
+  }
+
   $('*').animate({
     opacity: 1
   }, 400)
