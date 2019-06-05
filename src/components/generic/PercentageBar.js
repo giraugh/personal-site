bfaebf6068b7335f367872a9f9c37ce2bf2e8054
retroverse <ebreakey@gmail.com>
2019-06-05 12:27:37 +1000

    star bars

--- src/components/generic/PercentageBar.js
+++ src/components/generic/PercentageBar.js
@@ -4,16 +4,21 @@ import '../../styles/generic/PercentageBar'
 
 // accepts props, {percentage, label}
 
+const maxStars = 5
+
 export default class PercentageBar extends Component {
   render () {
-    const p = Math.floor(this.props.percentage * 100)
-    const cols = `${p}fr ${100 - p}fr`
+    const onStars = Math.floor(maxStars * this.props.percentage)
+    const offStars = maxStars - onStars
     return (
-      <div {...this.props} className={`percentage-bar ${this.props.className}`} style={{gridTemplateColumns: cols}}>
-        <div className='percentage-bar-complete percentage-bar-segment'>
+      <div {...this.props} className={`percentage-bar ${this.props.className}`}>
+        <div className='percentage-bar-content'>
           <span className='percentage-bar-label'> {this.props.label} </span>
+          <span className='stars-container'>
+            <span className='stars enabled'>{ '★'.repeat(onStars) }</span>
+            <span className='stars disabled'>{ '★'.repeat(offStars) }</span>
+          </span>
         </div>
-        <div className='percentage-bar-remaining percentage-bar-segment' />
       </div>
     )
   }
