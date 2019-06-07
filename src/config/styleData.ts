5e3798cab7d1b28587d918c0d188137ac48ec36b
retroverse <ebreakey@gmail.com>
2019-06-07 20:46:23 +1000

    New profile picture

--- src/config/styleData.ts
+++ src/config/styleData.ts
@@ -3,7 +3,7 @@ import { StyleData } from './styleData-t';
 const data : StyleData = {
   pageTitle: 'Ewan Breakey',
   bannerImageURL: 'http://retroverse.github.io/homepage-resources/img/banner/3.jpg',
-  profilePictureImageURL: 'http://images1.wikia.nocookie.net/__cb20130418050039/jjba/images/3/34/Gappy.jpg'
+  profilePictureImageURL: 'http://retroverse.github.io/homepage-resources/img/profile/profile_icon_white.png'
 }
 
 export default data
